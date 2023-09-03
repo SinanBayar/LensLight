ecc9d15829b345f1e35b32db3b07829821764350
Sinan Bayar <sinanbayar88@gmail.com>
2023-09-03 18:16:05 +0300

    Lesson 09

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import conn from "./db.js";
 
 import pageRoute from "./routes/pageRoute.js";
 import photoRoute from "./routes/photoRoute.js";
+import userRoute from "./routes/userRoute.js";
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ const port = process.env.PORT;
 // Static Files Middleware
 app.use(express.static("public"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Template Engine
 app.set("view engine", "ejs");
@@ -23,6 +25,7 @@ app.set("view engine", "ejs");
 // Routes
 app.use("/", pageRoute);
 app.use("/photos", photoRoute);
+app.use("/users", userRoute);
 
 app.listen(port, () => {
   console.log(`Application running on port: ${port}`);
