25b08c940558050bf988fc21ec569f4782831151
Sinan Bayar <sinanbayar88@gmail.com>
2023-09-13 00:23:29 +0300

    Lesson 16

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import conn from "./db.js";
 import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+import { v2 as cloudinary } from "cloudinary";
 
 import pageRoute from "./routes/pageRoute.js";
 import photoRoute from "./routes/photoRoute.js";
@@ -9,6 +11,12 @@ import userRoute from "./routes/userRoute.js";
 import { checkUser } from "./middlewares/authMiddleware.js";
 
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 // Connection to DB
 conn();
@@ -21,6 +29,7 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(fileUpload({ useTempFiles: true }));
 
 // Template Engine
 app.set("view engine", "ejs");
